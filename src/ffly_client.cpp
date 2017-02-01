4496390b0b7cef1523422fda232feb85313a0d56
MrDoomLittle <x-master-of-disaster-x@hotmail.com>
2017-02-01 11:46:11 +0000

    .

--- src/ffly_client.cpp
+++ src/ffly_client.cpp
@@ -5,13 +5,72 @@
 # include <cstdio>
 # include <boost/cstdint.hpp>
 
-# define WX_LEN 200
-# define WY_LEN 200
+# define WX_LEN 640
+# define WY_LEN 640
+
+float button_rcolour = 1.0;
+
+void draw() {
+	glClearColor(1.0, 1.0, 1.0, 1.0);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glOrtho(-1., 1., -1., 1., 1., 20.);
+
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    gluLookAt(0., 0., 10., 0., 0., 0., 0., 1., 0.);
+    glBegin(GL_QUADS);
+        glColor3f(1., 0., 0.); glVertex3f(-.75, -.75, 0.);
+        glColor3f(0., 1., 0.); glVertex3f( .75, -.75, 0.);
+        glColor3f(0., 0., 1.); glVertex3f( .75,  .75, 0.);
+        glColor3f(1., 1., 0.); glVertex3f(-.75,  .75, 0.);
+    glEnd();
+}
+
+void draw_button() {
+	glClearColor(1.0, 1.0, 1.0, 1.0);
+	//glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+
+	glBegin(GL_QUADS);
+		glColor3f(button_rcolour, 0.2, 0.3);
+		glVertex2f(-0.5f, -0.5f);
+		glVertex2f( 0.5f, -0.5f);
+		glVertex2f( 0.5f,  0.5f);
+		glVertex2f(-0.5f,  0.5f);
+	glEnd();
+}
+
+bool is_inside(int w_xpos, int w_ypos, int m_xpos, int m_ypos) {
+	if ((m_xpos > w_xpos && m_xpos < (w_xpos + WX_LEN)) && (m_ypos > w_ypos && m_ypos < (w_ypos + WY_LEN))) {
+		int container_x = w_xpos + (WX_LEN/4), container_y = w_ypos + (WY_LEN/4);
+
+		int container_xlen = (WX_LEN/2) + 1, container_ylen = (WY_LEN/2) + 1;
+
+		if ((m_xpos > container_x && m_xpos < (container_x + container_xlen)) && (m_ypos > container_y && m_ypos < (container_y + container_ylen))) {
+		
+			button_rcolour = 0.1;
+			return true;
+		}
+
+
+	}
+
+	button_rcolour = 1.0;
+	return false;
+}
+
+bool is_inside_window(int w_xpos, int w_ypos, int m_xpos, int m_ypos) {
+	if ((m_xpos > w_xpos && m_xpos < (w_xpos + WX_LEN)) && (m_ypos > w_ypos && m_ypos < (w_ypos + WY_LEN))) return true;
+	return false;
+}
 
 int main() {
 	XVisualInfo * _vis_info;
 
-	GLint att[] = { GLX_RGBA, GLX_DEPTH_SIZE, 24, GLX_DOUBLEBUFFER, None };
+	GLint att[] = {GLX_RGBA, GLX_DEPTH_SIZE, 24, GLX_DOUBLEBUFFER, None};
 
 	XSetWindowAttributes _win_att;
 
@@ -28,9 +87,9 @@ int main() {
 		return 1;
 	}
 
-	_rwindow = XRootWindow(_display, 0);
-
 	int _screen = DefaultScreen(_display);
+
+	_rwindow = XRootWindow(_display, 0);
 
 	_vis_info = glXChooseVisual(_display, 0, att);
 
@@ -61,23 +120,55 @@ int main() {
 	GLXContext _glx_context = glXCreateContext(_display, _vis_info, NULL, GL_TRUE);
 	glXMakeCurrent(_display, _window, _glx_context);
 
+	int unsigned screen_count = XScreenCount(_display);
+
+	Window * r_windows = static_cast<Window *>(malloc(screen_count * sizeof(Window)));
+
+	for (std::size_t i = 0; i != screen_count; i ++) {
+		r_windows[i] = XRootWindow(_display, i);
+	}
+
+	int w_xpos, w_ypos, m_xpos, m_ypos, r_mxpos, r_mypos;
+	int unsigned w_width, w_height, w_border_width, w_depth, m_mask;
+	Window child, root = XRootWindow(_display, 0);
 	for (;;) {
+		//XGetGeometry(_display, _window, &root, &w_xpos, &w_ypos, &w_width, &w_height, &w_border_width, &w_depth);
+		XTranslateCoordinates(_display, _window, root, 0, 0, &w_xpos, &w_ypos, &child);
+
+		for (std::size_t i = 0; i != screen_count; i ++) {
+			bool result = XQueryPointer(_display, r_windows[i], &_rwindow, &child, &r_mxpos, &r_mypos, &m_xpos, &m_ypos, &m_mask);
+			if (result) break;
+		}
+
+//		printf("window X: %d, window Y: %d, mouse X: %d, mouse Y: %d\n", w_xpos, w_ypos, m_xpos, m_ypos);
+
 		while (XPending(_display) > 0) {
 			XNextEvent(_display, &_xevent);
 			if (_xevent.type == KeyPress || _xevent.type == ClientMessage) goto end;
 		}
 
+		draw();
+		draw_button();
+
+		if (is_inside(w_xpos, w_ypos, m_xpos, m_ypos)) {
+		//	printf("currently inside window.\n");
+		}
+
 		// slow things down
-		for (int x = 0; x != 10000000; x ++) {}
+		for (int x = 0; x != 10000; x ++) {}
 
 
-		printf("tick.\n");
+//		printf("tick.\n");
 		glXSwapBuffers(_display, _window);
 	}
+
 	end:
 
+	glXMakeCurrent(_display, None, NULL);
+	glXDestroyContext(_display, _glx_context);
 	XDestroyWindow(_display, _window);
 	XCloseDisplay(_display);
+	std::free(r_windows);
 
 	return 0;
 }
