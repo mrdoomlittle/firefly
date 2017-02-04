9dce93163955aee95b8b0c7e6f44313b6e85c96a
MrDoomLittle <x-master-of-disaster-x@hotmail.com>
2017-02-04 20:39:53 +0000

    .

--- bsd_test/main.cpp
+++ bsd_test/main.cpp
@@ -8,16 +8,52 @@
 # include <string.h>
 using namespace std;
 int main() {
+	int sock, len, n;
+	struct sockaddr_in server;
+	struct sockaddr_in from;
+	int const buff_len = 128;
+	char buff[buff_len] = {'\0'};
+	socklen_t fromlen;
+
+	int port_num = 21299;
+
+	sock = socket(AF_INET, SOCK_DGRAM, 0);
+
+	if (sock < 0) {
+		printf("error can't open socket.\n");
+
+	}
+
+	len = sizeof(server);
+	bzero(&server, len);
+
+	server.sin_family = AF_INET;
+    server.sin_addr.s_addr = htons(INADDR_ANY);
+    server.sin_port = htons(port_num);
+
+	if (bind(sock, (struct sockaddr*)&server, len) < 0) {
+        printf("error binding socket.\n");
+        return 1;
+    }
+
+	fromlen = sizeof(struct sockaddr_in);
+
+	while(true) {
+		n = recvfrom(sock, buff, buff_len, 0, (struct sockaddr*)&from, &fromlen);
+		n = sendto(sock, buff, buff_len, 0, (struct sockaddr*)&from, fromlen);
+	}
+
+/*
 	int client, server;
 	int port_num = 21299;
 	int const buff_len = 128;
 	char buff[buff_len] = {'\0'};
+	struct sockaddr server_addr;
 
-	struct sockaddr_in server_addr;
 	socklen_t len;
 
 
-	client = socket(AF_INET, SOCK_STREAM, 0);
+	client = socket(AF_INET, SOCK_DGRAM, 0);
 
 	if (client < 0) {
 		printf("error establishing connection.\n");
@@ -35,15 +71,17 @@ int main() {
 
 	len = sizeof(server_addr);
 
-	listen(client, 1);
+	//listen(client, 1);
 
-	server = accept(client, (struct sockaddr*)&server_addr, &len);
-	if (server < 0) {
-		printf("error accepting.\n");
-	}
+	//server = accept(client, (struct sockaddr*)&server_addr, &len);
+	//if (server < 0) {
+	//	printf("error accepting.\n");
+	//}
 
 	while (server > 0) {
 		strcpy(buff, "test");
-		send(server, buff, buff_len, 0);
+		recvfrom(client, buff, buff_len, 0, &server_addr, sizeof(server_addr))
+		//send(server, buff, buff_len, 0);
 	}
+*/
 }
