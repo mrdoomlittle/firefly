4d68189b13b1ba333862a3fb787c1b7f1fa8733b
MrDoomLittle <x-master-of-disaster-x@hotmail.com>
2017-08-17 12:40:51 +0100

    .

--- src/maths/rotate_point.cpp
+++ src/maths/rotate_point.cpp
@@ -1,28 +1,17 @@
 # include "rotate_point.hpp"
-# include <cstdio>
-void mdl::firefly::maths::rotate_point(uint_t& __xaxis, uint_t& __yaxis, boost::uint16_t __angle, uint_t __xoffset, uint_t __yoffset, boost::int8_t __dir) {
-	float rad = (float)__angle * (PI / 180.0f);
-	float sn = sinf(rad), cs = cosf(rad);
+void mdl::firefly::maths::rotate_point(int_t& __xaxis, int_t& __yaxis, u16_t __angle, i8_t __dir) {
+	double rad = (double)__angle*(PI/180.0f);
+	double sn = sin(rad), cs = cos(rad);
 
-	float n_xaxis, n_yaxis;
+	double n_xaxis, n_yaxis;
 	if (__dir == -1) {
-		n_xaxis = (float)__xaxis * cs + (float)__yaxis * sn;
-		n_yaxis = -(float)__xaxis * sn + (float)__yaxis * cs;
+		n_xaxis = (double)__xaxis*cs+(double)__yaxis*sn;
+		n_yaxis = -(double)__xaxis*sn+(double)__yaxis*cs;
 	} else if (__dir == 1) {
-		n_xaxis = -(float)__xaxis * cs + (float)__yaxis * sn;
-		n_yaxis = (float)__xaxis * sn + (float)__yaxis * cs;
+		n_xaxis = -(double)__xaxis*cs+(double)__yaxis*sn;
+		n_yaxis = (double)__xaxis*sn+(double)__yaxis*cs;
  	}
 
-	if (n_xaxis < 0.0f) {
-		n_xaxis -= n_xaxis * 2;
-		n_xaxis = (float)__xoffset - n_xaxis;
-	} else n_xaxis = (float)__xoffset + n_xaxis;
-
-	if (n_yaxis < 0.0f) {
-		n_yaxis -= n_yaxis * 2;
-		n_yaxis = (float)__yoffset - n_yaxis;
-	} else n_yaxis = (float)__yoffset + n_yaxis;
-
-	__xaxis = (uint_t)roundf(n_xaxis);
-	__yaxis = (uint_t)roundf(n_yaxis);
+	__xaxis = (int_t)round(n_xaxis);
+	__yaxis = (int_t)round(n_yaxis);
 }
--- src/maths/rotate_point.hpp
+++ src/maths/rotate_point.hpp
@@ -7,11 +7,7 @@
 namespace mdl {
 namespace firefly {
 namespace maths {
-
-// NOTE: xaxis and yaxis is for the thing your rotating not where it is in the world
-// e.g. if i was to rotate a rect 8x6 i would start from 0 to etc.
-// other thing the offset should we where it is in the world.
-void rotate_point(uint_t& __xaxis, uint_t& __yaxis, boost::uint16_t __angle, uint_t __xoffset, uint_t __yoffset, boost::int8_t __dir);
+void rotate_point(int_t& __xaxis, int_t& __yaxis, u16_t __angle, i8_t __dir);
 }
 }
 }
