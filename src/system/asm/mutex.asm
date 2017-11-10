233741bbe05424ff9ca28c2273a02f52ff536cbd
Daniel Robson <danielijrobson@gmail.com>
2017-11-10 22:28:43 +0000

    .

--- src/system/asm/mutex.asm
+++ src/system/asm/mutex.asm
@@ -2,23 +2,16 @@ section .text
 global ffly_mutex_lock
 global ffly_mutex_unlock
 ffly_mutex_lock:
-; check until not locked
-	_re_check:
 	mov al, [rdi]
 	cmp al, 1
-	je _re_check
+	je ffly_mutex_lock
 
-; incrment to a locked value
-	lock inc BYTE[rdi]
-; check if correct else try again to lock
-	cmp BYTE[rdi], 1
+	lock xchg [rdi], al
+	cmp al, 0
 	jne ffly_mutex_lock
 	ret
 ffly_mutex_unlock:
-	mov al, [rdi]
-; check if locked, if not skip the decrementation instruction
-	cmp al, 1
-	jne _sk_decr
-	lock dec BYTE[rdi]
+	mov dl, [rdi]
+	lock xor BYTE[rdi], al
 	_sk_decr:
 	ret
