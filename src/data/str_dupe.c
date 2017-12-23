6d901e9fd0def29ac1d9613eca10de5b01fcaffe
Daniel Robson <danielijrobson@gmail.com>
2017-12-23 18:26:56 +0000

    fix

--- src/data/str_dupe.c
+++ src/data/str_dupe.c
@@ -4,6 +4,6 @@
 # include "../ffly_def.h"
 char* ffly_str_dupe(char *__s) {
 	void *p = NULL;
-	ffly_mem_dupe(&p, __s, ffly_str_len(__s));
+	ffly_mem_dupe(&p, __s, ffly_str_len(__s)+1);
 	return (char*)p;
 }
--- src/data/str_len.c
+++ src/data/str_len.c
@@ -2,6 +2,6 @@
 mdl_uint_t ffly_str_len(char *__s) {
 	if (!__s) return 0;
 	char *itr = __s;
-	while(*(++itr) != '\0');
+	while(*(itr++) != '\0');
 	return itr-__s;
 }
