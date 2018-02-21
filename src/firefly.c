1131203d26f1ebd2d964af6cef9712b9c053f75e
Daniel Robson <danielijrobson@gmail.com>
2018-02-21 13:51:06 +0000

    .

--- src/firefly.c
+++ src/firefly.c
@@ -13,45 +13,40 @@ extern void(*ffly_freep)(void*);
 extern void*(*ffly_reallocp)(void*, mdl_uint_t);
 */
 ffly_err_t ffmain(int, char const**);
-char const static *desc = "firefly ????";
+char const static *by = "mrdoomlittle";
+# include "data/str_cpy.h"
 void _start(void) {
 	int long argc;
 	char const **argv;
-	__asm__("mov 24(%%rsp), %0\t\n"
-			"mov %%rsp, %%rax\n\t"
-			"add $32, %%rax\n\t"
+	__asm__("mov 8(%%rbp), %0\t\n"
+			"mov %%rbp, %%rax\n\t"
+			"add $16, %%rax\n\t"
 			"mov %%rax, %1" : "=r"(argc), "=r"(argv) :);
 	void *frame;
 	ffly_ar_init();
 	frame = ffly_frame();
 	void **p = ffly_alloca(sizeof(void*), NULL);
  	ffly_io_init();
+
+	if (argc < 2) {
+		//ffly_printf("please provide sysconf.\n");
+		//goto _end;
+	}
+
+	//ffly_ld_sysconf(argv[1]);
 	ffly_arcs_init();
 
-	*p = desc;
+	*p = (void*)by;
 	ffly_arcs_creatarc("info");
 	ffly_arcs_tun("info");
-	ffly_arcs_creatrec("desc", p, _ffly_rec_def, 0);
+	ffly_arcs_creatrec("created-by", p, _ffly_rec_def, 0);
 	ffly_arcs_bk();
 
-	/*
-	ffly_allocp = ffly_alloc;
-	ffly_freep = ffly_free;
-	ffly_reallocp = ffly_realloc;
-	*/
-	/*
-	if (argc < 2) {
-		ffly_printf("please provide sysconf.\n");
-		goto _end;
-	}
-	ffly_ld_sysconf(*(char const**)(argv+1));
-	ffmain(argc, (char const**)argv);
-	_end:
-	ffly_free_sysconf();
-	ffly_thread_cleanup();
-	*/
-
 	ffmain(argc, argv);
+	ffly_printf("\n\n");
+
+	//ffly_free_sysconf();
+	_end:
 	ffly_arstat();
 	ffly_arcs_de_init();
 	ffly_io_closeup();
