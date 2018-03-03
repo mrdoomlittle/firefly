404aa53acf58c0f79f6c868fa3488a4afca895cd
Daniel Robson <danielijrobson@gmail.com>
2018-03-03 19:28:33 +0000

    .

---
+++ src/mul/main.c
@@ -0,0 +1,36 @@
+# include "mul.h"
+# include "../stdio.h"
+# include "../string.h"
+# include "../types/err_t.h"
+ffly_err_t ffmain(int __argc, char const *__argv[]) {
+	char const *src = NULL, *dst = NULL;
+
+	char const **arg = __argv;
+	char const **end = arg+__argc;
+	arg++; // ignore
+	while(arg != end) {
+		if (!strcmp(*arg, "-d"))
+			dst = *(++arg);
+		else if (!strcmp(*arg, "-s"))
+			src = *(++arg);
+		arg++;
+	}
+
+	if (!src || !dst) {
+		fprintf(stderr, "usage: -s source -d destination\n");
+		return -1;
+	}
+
+	struct mul m;
+	mul_prepare(&m);
+	mul_ld(&m, src);
+	mul_process(&m);
+
+//rickp a = get_brick(&m, "id_3");
+//har r = '2';
+//ul_brickw(&r, 4, 1, a);
+
+	mul_oust(&m, dst);
+	mul_cleanup(&m);
+	return 0;
+}
--- src/string.h
+++ src/string.h
@@ -7,4 +7,10 @@
 # include "dep/mem_cpy.h"
 # define memcpy(__dst, __src, __n) \
 	ffly_mem_cpy(__dst, __src, __n)
+# define strlen(__s) \
+	ffly_str_len(__s)
+# define memcmp(__p0, __p1, __n) \
+	ffly_mem_cmp(__p0, __p1, __n)
+# define strcmp(__s0, __s1) \
+	ffly_str_cmp(__s0, __s1)
 # endif /*__ffly__string__h*/
