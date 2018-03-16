7965efc0237820fab6ac39463ae0822ade6e6282
Daniel Robson <danielijrobson@gmail.com>
2018-03-16 21:17:37 +0000

    wrong pop sequence

--- src/dep/str_dup.s
+++ src/dep/str_dup.s
@@ -7,13 +7,13 @@
 *	rax - ret
 */
 __ffly_str_dup:
-	push %rdi
-	call __ffly_str_len
-	pop %rdi
-	
 	push %rbp
 	movq %rsp, %rbp
 	sub $8, %rsp
+
+	push %rdi
+	call __ffly_str_len
+	pop %rdi
 
 	push %rsi
 	push %rbx
@@ -27,10 +27,13 @@ __ffly_str_dup:
 	push %rbx
 	call __ffly_mem_dup
 	pop %rbx
+//	dec %rbx
 
 	movq -8(%rbp), %rax
-	pop %rsi
+
 	pop %rbx
+	pop %rsi
+
 	movq %rbp, %rsp
 	pop %rbp
 	ret
