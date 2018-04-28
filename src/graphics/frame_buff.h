fdc1fbc85460f4a8fdddaa4454938e552ca148d7
Daniel Robson <danielijrobson@gmail.com>
2018-04-28 01:37:49 +0100

    .

---
+++ src/graphics/frame_buff.h
@@ -0,0 +1,9 @@
+# ifndef __ffly__frame__buff__h
+# define __ffly__frame__buff__h
+
+/*
+*   we dont want to directly use the window frame buffer,
+*   as we want to be able to pipe the frame over to some where else if needed.
+*/
+
+# endif /*__ffly__frame__buff__h*/
